238ddd5bd5faf55d9d43fb9f11c5e87e1e397980
Rodrigo <rodrigokaconde@yahoo.com.br>
2021-12-27 22:24:05 -0300

    commit init

---
+++ loan-api/src/test/java/br/com/rfoliveira/loanapi/LoanApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rfoliveira.loanapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
