1da8cc56c61bad39fb77cd4d1d648d7f72233498
Rodrigo <rodrigokaconde@yahoo.com.br>
2021-12-29 13:47:26 -0300

    configure validation and password encryption

--- client-api/src/main/java/br/com/rfoliveira/clientapi/service/ClientService.java
+++ client-api/src/main/java/br/com/rfoliveira/clientapi/service/ClientService.java
@@ -5,7 +5,9 @@ import br.com.rfoliveira.clientapi.model.Loan;
 import br.com.rfoliveira.clientapi.model.response.LoanClientResponse;
 import br.com.rfoliveira.clientapi.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -15,13 +17,15 @@ import java.util.Optional;
 @Service
 public class ClientService {
     private final ClientRepository clientRepository;
-
+    private final PasswordEncoder encoder;
     @Autowired
-    public ClientService (ClientRepository clientRepository){
+    public ClientService(ClientRepository clientRepository, PasswordEncoder encoder){
         this.clientRepository = clientRepository;
+        this.encoder = encoder;
     }
 
     public Client registerClient (Client client){
+        client.setPassword(encoder.encode(client.getPassword()));
         return clientRepository.save(client);
     }
 
@@ -43,7 +47,12 @@ public class ClientService {
 
     public ResponseEntity<Boolean> loginClient(String email, String password) {
         Optional<Client> findClient = clientRepository.findByEmail(email);
-        Boolean valid = findClient.get().getPassword().equals(password);
-        return ResponseEntity.ok().body(valid);
+        if(findClient.isEmpty()){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(false);
+        }
+
+        Boolean valid = encoder.matches(password, findClient.get().getPassword());
+        HttpStatus status = valid ? HttpStatus.OK : HttpStatus.UNAUTHORIZED;
+        return ResponseEntity.status(status).body(valid);
     }
 }
